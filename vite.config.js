abf5b68387da4253ccc5a86af271c763e19026d3
athumaniMfaume <athumanimfaume1995@gmail.com>
2025-07-01 16:28:32 +0300

    Deploy built React app

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    assetsInclude: ['**/*.PNG'],
+  base: './', // <-- Add this line
+  assetsInclude: ['**/*.PNG'],
   plugins: [react()],
 })
+
